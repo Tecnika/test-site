b5efe79077d2705a54291f21bb4c4c2d6ee9cc35
NK <nika.koledova@yandex.ru>
2023-02-09 15:33:22 +0300

    upd header

--- src/component/header.js
+++ src/component/header.js
@@ -5,9 +5,11 @@ import '../css/css.css'
 export default function Header({ children, n_class, childClass }) {
     return (
             <div className="header">
+                <div className="content header">
                 <Link to='/blog/'>Blog</Link>
                 <Link to="/" className="title">SiteName</Link> 
                 <Link to="/news/">News</Link>
+                </div>
             </div>
     )
 }
